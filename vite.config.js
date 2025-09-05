751b7a69f8596741ffb1df55c0b8cbbbc62315a2
1011gnn1118 <fukudomenou@gmail.com>
2025-09-05 23:28:41 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ★これを追加（相対パスにする）
-})
+  base: "/sora-prompt-builder/" // ← ここを '/リポ名/' に
+});
